2db31624c8c5d3cde248ba78ad0f3eca4badadab
MohamedAli Sadhakathullah <112577763+sfc-gh-msadhakathullah@users.noreply.github.com>
2026-04-02 16:17:02 -0500

    Update main_coco.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
-terraform {
-  cloud {
-    organization = "My_Devops_Test"
+    terraform {
+      cloud {
+        organization = "My_Devops_Test"
 
-    workspaces {
-      name = "gh-actions-demo"
+        workspaces {
+          name = "gh-actions-demo"
+        }
+      }
+
+      required_providers {
+        snowflake = {
+          source  = "snowflake-labs/snowflake"
+          version = "~> 1.0"
+        }
+      }
     }
-  }
 
-  required_providers {
-    snowflake = {
-      source  = "chanzuckerberg/snowflake"
-      version = "0.25.17"
+    provider "snowflake" {}
+
+    resource "snowflake_database" "demo_db" {
+      name    = "DEMO_DB"
+      comment = "Database for Snowflake Terraform demo"
     }
-  }
-}
-
-provider "snowflake" {}
-
-resource "snowflake_database" "demo_db" {
-  name    = "DEMO_DB"
-  comment = "Database for Snowflake Terraform demo"
-}
