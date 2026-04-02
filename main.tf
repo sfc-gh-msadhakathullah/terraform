68759a7aae52508b20a07ed7dd308dacde47b744
MohamedAli Sadhakathullah <112577763+sfc-gh-msadhakathullah@users.noreply.github.com>
2026-04-02 16:18:28 -0500

    Update main_fmt.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
-    terraform {
-      cloud {
-        organization = "My_Devops_Test"
+terraform {
+  cloud {
+    organization = "My_Devops_Test"
 
-        workspaces {
-          name = "gh-actions-demo"
-        }
-      }
-
-      required_providers {
-        snowflake = {
-          source  = "snowflake-labs/snowflake"
-          version = "~> 1.0"
-        }
-      }
+    workspaces {
+      name = "gh-actions-demo"
     }
+  }
 
-    provider "snowflake" {}
-
-    resource "snowflake_database" "demo_db" {
-      name    = "DEMO_DB"
-      comment = "Database for Snowflake Terraform demo"
+  required_providers {
+    snowflake = {
+      source  = "snowflake-labs/snowflake"
+      version = "~> 1.0"
     }
+  }
+}
+
+provider "snowflake" {}
+
+resource "snowflake_database" "demo_db" {
+  name    = "DEMO_DB"
+  comment = "Database for Snowflake Terraform demo"
+}
